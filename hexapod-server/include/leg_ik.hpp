ab311d4691e38f6b039424c065741cf5e4b9ba53
rpharmer <volly55@live.co.uk>
2026-03-12 01:39:50 +0000

    added leg/hexapod geometry holder to ik class

--- hexapod-server/include/leg_ik.hpp
+++ hexapod-server/include/leg_ik.hpp
@@ -11,8 +11,5 @@ private:
     bool solveOneLeg(const LegRawState& est, LegRawState& out, const FootTarget& foot);
     uint64_t seq_tx_{0};
 
-    // Toy geometry
-    double l1_{0.10}; // hip offset / ignored in this simplified model
-    double l2_{0.18}; // upper leg
-    double l3_{0.18}; // lower leg
+    HexapodGeometry hexGeo;
 };
