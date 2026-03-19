a6b58f8b1e555450d1ebdd55b54144a0d90e9378
rpharmer <volly55@live.co.uk>
2026-03-19 01:44:52 +0000

    Harden DoubleBuffer against concurrent read/write races

--- hexapod-server/include/double_buffer.hpp
+++ hexapod-server/include/double_buffer.hpp
@@ -1,17 +1,20 @@
 #pragma once
 
 #include <atomic>
+#include <mutex>
 
 template <typename T>
 class DoubleBuffer {
 public:
     void write(const T& value) {
+        std::lock_guard<std::mutex> lock(mutex_);
         const int next = 1 - index_.load(std::memory_order_relaxed);
         buffers_[next] = value;
         index_.store(next, std::memory_order_release);
     }
 
     T read() const {
+        std::lock_guard<std::mutex> lock(mutex_);
         const int idx = index_.load(std::memory_order_acquire);
         return buffers_[idx];
     }
@@ -19,4 +22,5 @@ public:
 private:
     T buffers_[2]{};
     std::atomic<int> index_{0};
+    mutable std::mutex mutex_;
 };
