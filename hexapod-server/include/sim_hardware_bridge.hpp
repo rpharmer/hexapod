639abe1fe943ca21fa2b106b3c40c796c3a3ab40
rpharmer <volly55@live.co.uk>
2026-03-20 01:49:45 +0000

    Add simulated hardware bridge implementing IHardwareBridge

---
+++ hexapod-server/include/sim_hardware_bridge.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <array>
+#include <optional>
+
+#include "hardware_bridge.hpp"
+
+struct SimHardwareFaultToggles {
+    bool drop_bus{false};
+    bool low_voltage{false};
+    bool high_current{false};
+    std::optional<std::array<bool, kNumLegs>> forced_contacts{};
+
+    float nominal_voltage{12.0f};
+    float low_voltage_value{6.0f};
+    float nominal_current{1.0f};
+    float high_current_value{25.0f};
+};
+
+class SimHardwareBridge final : public IHardwareBridge {
+public:
+    explicit SimHardwareBridge(SimHardwareFaultToggles fault_toggles = {},
+                               DurationSec read_cycle_period = DurationSec{0.02},
+                               DurationSec response_time_constant = DurationSec{0.08});
+    ~SimHardwareBridge() override = default;
+
+    bool init() override;
+    bool read(RawHardwareState& out) override;
+    bool write(const JointTargets& in) override;
+
+private:
+    static double clamp01(double value);
+
+    SimHardwareFaultToggles fault_toggles_{};
+    DurationSec read_cycle_period_{};
+    DurationSec response_time_constant_{};
+
+    bool initialized_{false};
+    RawHardwareState state_{};
+    JointTargets commanded_targets_{};
+};
