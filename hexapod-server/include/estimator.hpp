312b77ebfa29336f04c3c88c90f5210e2ac1f408
rpharmer <volly55@live.co.uk>
2026-03-22 23:35:33 +0000

    Expand SimpleEstimator with ground-plane body twist estimate

--- hexapod-server/include/estimator.hpp
+++ hexapod-server/include/estimator.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <array>
+
 #include "types.hpp"
 
 class IEstimator {
@@ -11,4 +13,12 @@ public:
 class SimpleEstimator final : public IEstimator {
 public:
     EstimatedState update(const RawHardwareState& raw) override;
+
+private:
+    static constexpr uint64_t kContactMemoryWindowUs = 250000;
+
+    std::array<Vec3, kNumLegs> last_contact_points_body_m_{};
+    std::array<TimePointUs, kNumLegs> last_contact_timestamps_{};
+    TimePointUs last_twist_timestamp_{};
+    Vec3 last_twist_pos_rad_{};
 };
